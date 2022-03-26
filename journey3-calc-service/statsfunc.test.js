2e153fa3db75eafaaaf461ead4dc6cf9007ac20b
artemkv <artemkv@gmail.com>
2022-03-26 12:08:38 +0100

    Unit-tests improved

--- journey3-calc-service/statsfunc.test.js
+++ journey3-calc-service/statsfunc.test.js
@@ -3,9 +3,19 @@
 const statsfunc = require('./statsfunc');
 
 test('getDaysSince', () => {
-  expect(statsfunc.getDaysSince('2022-03-19T23:25:28.924017Z', '2022-03-23T17:30:50.888718Z')).toBe(3);
+  // same day but later
   expect(statsfunc.getDaysSince('2022-03-23T17:30:50.888718Z', '2022-03-23T23:25:28.924017Z')).toBe(0);
+  // next day but not before full 24 hours
+  expect(statsfunc.getDaysSince('2022-03-23T23:25:28.924017Z', '2022-03-24T17:30:50.888718Z')).toBe(0);
+  // next day and more than 24 hours later
   expect(statsfunc.getDaysSince('2022-03-22T17:30:50.888718Z', '2022-03-23T23:25:28.924017Z')).toBe(1);
+  // more than 1 day difference
+  expect(statsfunc.getDaysSince('2022-03-19T23:25:28.924017Z', '2022-03-23T17:30:50.888718Z')).toBe(3);
+
+  // 1 second before full 24 hours
+  expect(statsfunc.getDaysSince('2022-03-23T17:30:50.888718Z', '2022-03-24T17:30:49.888718Z')).toBe(0);
+  // just 1 second over 24 hours
+  expect(statsfunc.getDaysSince('2022-03-23T17:30:50.888718Z', '2022-03-24T17:30:51.888718Z')).toBe(1);
 });
 
 test('getRetentionBucket', () => {

944eeb9e2a429f2bae30b10649aa1174f592dd44
artemkv <artemkv@gmail.com>
2022-07-28 20:46:28 +0200

    added description to front page

--- journey3-website/src/components/PublicArea.js
+++ journey3-website/src/components/PublicArea.js
@@ -16,6 +16,15 @@ export default (props) => {
         </div>
         <div className="row">
             <h1 className="center-align">Journey 3</h1>
+            <p className='center'><em>Lightweight Anonymous Mobile Analytics</em></p>
+            <ul className='center'>
+                <li>Sessions</li>
+                <li>Unique users</li>
+                <li>New Users</li>
+                <li>Feature Usage</li>
+                <li>Stage Conversions</li>
+                <li>Retention</li>
+            </ul>
         </div>
     </div>;
 };
